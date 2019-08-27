53e80ff77191dcc906e5e490405d62ed8cf38cd1
daogangtang <daogangtang@gmail.com>
2019-08-27 19:09:32 +0800

    first commit.
    Signed-off-by: daogangtang <daogangtang@gmail.com>

---
+++ start_light.sh
@@ -0,0 +1,7 @@
+substrate --base-path /tmp/tgg \
+  --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/QmRpheLN4JWdAnY7HGJfWFNbfkQCb6tFf4vvA6hgjMZKrR \
+  --chain=local \
+  --port 30335 \
+  --telemetry-url ws://telemetry.polkadot.io:1024 \
+  --light
+
